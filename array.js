b8bf1c899ae04f98ee94ef6b2dd8b9eb1a84eed9
saarmann <kaspar.saarmann@gmail.com>
2019-12-10 17:00:13 +0200

    [DAY-2]: Excercise 2

---
+++ array.js
@@ -0,0 +1,11 @@
+function arrayProbability() {
+    let mySymbolArray = [A. B, C, D, E];
+    
+    let possibility = Math.random() * 100;
+    let probability = possibility / mySymbolArray.length;
+
+    let probabilityArray = [];
+    
+    
+
+}
