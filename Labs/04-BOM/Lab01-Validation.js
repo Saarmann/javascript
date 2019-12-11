4377547eedb1943ab58319443667a453f06f7042
saarmann <kaspar.saarmann@gmail.com>
2019-12-11 16:41:34 +0200

    [DAY-03]: validate form

--- Labs/04-BOM/Lab01-Validation.js
+++ Labs/04-BOM/Lab01-Validation.js
@@ -19,22 +19,83 @@ function validateInputName () {
 
        document.getElementById("txtName").className = "error span6";
        document.getElementById("txtError").style = "visibility: visible";  
-
-        document.getElementById("txtName").appendChild(newSpan);
+       document.getElementById("txtName").appendChild(newSpan);
 
     } else {
         document.getElementById("txtName").className = "span6 valid";
 }       document.getElementById("txtError").style = "visibility: hidden";
 }
 
-// function setSpan() {
-    
-//     let parent = document.getElementsByClassName("span 9");
-//     let newSpan = document.createElement('span');
-    
-//     newSpan.setAttribute(´"class", "alert alert-error");
-//     newSpan.innerHTML = "<strong>Error!</strong> Not a Number";
+function validateAge () {
+   
+    let inputAge = document.getElementById("txtAge").value;
+    let input = parseInt(inputAge);
+ 
+     if (isNaN(input)) {
+ 
+        document.getElementById("txtAge").className = "error span6";
+        document.getElementById("ageError").style = "visibility: visible";  
+ 
+     } else {
+         document.getElementById("txtAge").className = "span6 valid";
+ }       document.getElementById("ageError").style = "visibility: hidden";
+ }
 
-//     parent.insertBefore()
+ function validateEmail () {
+   
+    let input = document.getElementById("txtEmail").value;
+ 
+    if (input == "" ) {
+ 
+        document.getElementById("txtEmail").className = "error span6";
+        document.getElementById("emailError").style = "visibility: visible";
+        document.getElementById("txtName").appendChild(newSpan);  
+ 
+     } else {
+         document.getElementById("txtEmail").className = "span6 valid";
+ }       document.getElementById("emailError").style = "visibility: hidden";
+ }
 
-// }
+ function validatePassword () {
+   
+    let input = document.getElementById("txtPassword").value;
+ 
+    if (input == "" ) {
+ 
+        document.getElementById("txtPassword").className = "error span6";
+        document.getElementById("passwordError").style = "visibility: visible";
+        document.getElementById("txtName").appendChild(newSpan);  
+ 
+     } else {
+         document.getElementById("txtPassword").className = "span6 valid";
+ }       document.getElementById("passwordError").style = "visibility: hidden";
+ }
+
+ function validatePassword () {
+   
+    let input = document.getElementById("txtRepeatPassword").value;
+ 
+    if (input == "" ) {
+ 
+        document.getElementById("txtRepeatPassword").className = "error span6";
+        document.getElementById("repeatPasswordError").style = "visibility: visible";
+        document.getElementById("txtName").appendChild(newSpan);  
+ 
+     } else {
+         document.getElementById("txtRepeatPassword").className = "span6 valid";
+ }       document.getElementById("repeatPasswordError").style = "visibility: hidden";
+ }
+ function validateId () {
+   
+    let inputAge = document.getElementById("txtIdCode").value;
+    let input = parseInt(inputAge);
+ 
+     if (isNaN(input)) {
+ 
+        document.getElementById("txtIdCode").className = "error span6";
+        document.getElementById("idCodeError").style = "visibility: visible";  
+ 
+     } else {
+         document.getElementById("txtIdCode").className = "span6 valid";
+ }       document.getElementById("idCodeError").style = "visibility: hidden";
+ }
