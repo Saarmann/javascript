20fe029c83eece0d3578f21743e200de4a98c11a
saarmann <kaspar.saarmann@gmail.com>
2019-12-16 17:04:22 +0200

    [DAY-06] Table Excercise

--- Labs/04-BOM/Lab03-Tables.js
+++ Labs/04-BOM/Lab03-Tables.js
@@ -1,16 +1,26 @@
 alert ("Implement solution in Lab03-Tables.js file ");
 alert ("Test data connected: OrderNo: "+shipments[0].orderNo);
 
+var rows = 10;
+
 function loadTable() {
 
 let myTable = document.getElementById("myTable");
 const buttons = "<a href=\"#\" class=\"btn-action glyphicons eye_open btn-info\"><i></i></a><a href=\"#\" class=\"btn-action glyphicons pencil btn-success\"><i></i></a><a href=\"#\" class=\"btn-action glyphicons remove_2 btn-danger\"><i></i></a>"
-
-let tableRows = 10;
-//document.getElementById("nextButton").addEventListener("click", tableRows = tableRows + 10 );
-let i = tableRows - 10;
-let maxTableRows = shipments.length;
+//document.getElementById("nextButton").addEventListener("click", );
  
+
+let maxTableRows = shipments.length;
+var tableRows;
+
+if ((rows - 10) > (200 - 10)) {
+    var tableRows = maxTableRows;
+    var i = maxTableRows - 10;
+} else {
+    var tableRows = rows
+    var i = rows - 10;
+}
+
 var columns = [];
 for (let i = 0; i < shipments.length; i++) {
     for(let key in shipments[i]){
@@ -23,6 +33,7 @@ for (let i = 0; i < shipments.length; i++) {
 // create table rows
 let tableRow = myTable.insertRow(); 
     
+
     for (i; i < tableRows; i++) {
 
         tableRow = myTable.insertRow();
@@ -38,7 +49,85 @@ let tableRow = myTable.insertRow();
         }          
     }    
 
-    
 
 }
 
+function loadTableNextPage() {
+
+let myTable = document.getElementById("myTable");
+const buttons = "<a href=\"#\" class=\"btn-action glyphicons eye_open btn-info\"><i></i></a><a href=\"#\" class=\"btn-action glyphicons pencil btn-success\"><i></i></a><a href=\"#\" class=\"btn-action glyphicons remove_2 btn-danger\"><i></i></a>"
+//document.getElementById("nextButton").addEventListener("click", );
+ 
+var rows = 150;
+let maxTableRows = shipments.length;
+var tableRows;
+
+if ((rows - 10) > (200 - 10)) {
+    var tableRows = maxTableRows;
+    var i = maxTableRows - 10;
+} else {
+    var tableRows = rows
+    var i = rows - 10;
+}
+
+var columns = [];
+for (let i = 0; i < shipments.length; i++) {
+    for(let key in shipments[i]){
+        if (columns.indexOf(key) === -1) {
+            columns.push(key);
+        }
+    }
+}
+
+// create table rows
+let tableRow = myTable.insertRow(); 
+    
+
+    for (i; i < tableRows; i++) {
+
+        tableRow = myTable.insertRow();
+
+        // fill cells in table
+        for(let j = 0; j < (columns.length+1); j++ ) {
+            let tableCell = tableRow.insertCell();
+            if(j == columns.length) {
+                tableCell.innerHTML = buttons;
+            } else {
+                tableCell.innerHTML = shipments[i][columns[j]];
+            }            
+        }          
+    }    
+
+}
+
+
+function nextPage() {
+
+    var rows = rows + 10;
+    loadTableNextPage();
+}
+
+function previousPage() {
+
+
+
+}
+
+function lastPage () {
+
+
+
+}
+
+function firstPage() {
+
+
+
+}
+
+function clearTable() {
+
+    document.getElementById("myTable");
+    
+
+}
