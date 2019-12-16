3188aa2dd3c7889184c0295d596796cc8421043b
saarmann <kaspar.saarmann@gmail.com>
2019-12-16 13:43:18 +0200

    [DAY-06]: Table excercise 2nd part

--- Labs/04-BOM/Lab03-Tables.js
+++ Labs/04-BOM/Lab03-Tables.js
@@ -6,6 +6,11 @@ function loadTable() {
 let myTable = document.getElementById("myTable");
 const buttons = "<a href=\"#\" class=\"btn-action glyphicons eye_open btn-info\"><i></i></a><a href=\"#\" class=\"btn-action glyphicons pencil btn-success\"><i></i></a><a href=\"#\" class=\"btn-action glyphicons remove_2 btn-danger\"><i></i></a>"
 
+let tableRows = 10;
+//document.getElementById("nextButton").addEventListener("click", tableRows = tableRows + 10 );
+let i = tableRows - 10;
+let maxTableRows = shipments.length;
+ 
 var columns = [];
 for (let i = 0; i < shipments.length; i++) {
     for(let key in shipments[i]){
@@ -13,13 +18,12 @@ for (let i = 0; i < shipments.length; i++) {
             columns.push(key);
         }
     }
-
 }
 
 // create table rows
 let tableRow = myTable.insertRow(); 
     
-    for (let i = 0; i < shipments.length; i++) {
+    for (i; i < tableRows; i++) {
 
         tableRow = myTable.insertRow();
 
@@ -31,12 +35,10 @@ let tableRow = myTable.insertRow();
             } else {
                 tableCell.innerHTML = shipments[i][columns[j]];
             }            
-       
-        }
-          
+        }          
     }    
 
+    
 
 }
-
 
