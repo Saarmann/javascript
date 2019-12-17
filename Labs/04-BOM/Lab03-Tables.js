1b4d5dd986c20caf7a848faad15e93f74f08588e
saarmann <kaspar.saarmann@gmail.com>
2019-12-17 10:04:01 +0200

    [DAY-07]: Table excercise code formating

--- Labs/04-BOM/Lab03-Tables.js
+++ Labs/04-BOM/Lab03-Tables.js
@@ -4,13 +4,15 @@ alert ("Test data connected: OrderNo: "+shipments[0].orderNo);
 let newRows = 0;
 
 function loadTable() {
+
     let myTable = document.getElementById("myTable");
     const buttons = "<a href=\"#\" class=\"btn-action glyphicons eye_open btn-info\"><i></i></a><a href=\"#\" class=\"btn-action glyphicons pencil btn-success\"><i></i></a><a href=\"#\" class=\"btn-action glyphicons remove_2 btn-danger\"><i></i></a>"
     let rows = newRows + 10;
     let maxTableRows = shipments.length;
     let tableRows;
     let i;
-    if ((rows - 10) > (200 - 10)) {
+
+    if ((rows - 10) > (maxTableRows - 10)) {
         tableRows = maxTableRows;
         i = maxTableRows - 10;
     } else {
@@ -45,30 +47,35 @@ function loadTable() {
         }
     }
 }
+
 function nextPage() {
     newRows = newRows + 10;
     clearTable();
     loadTable();
 }
+
 function previousPage() {
     newRows = newRows - 10;
     clearTable();
     loadTable();
 }
+
 function lastPage() {
     newRows = shipments.length + 10;
     clearTable();
     loadTable();
 }
+
 function firstPage() {
     newRows = 0;
     clearTable();
     loadTable();
 }
+
 function clearTable() {
     let myTable = document.getElementById("myTable");
     let rowCount = myTable.rows.length;
-    while (rowCount > 1) { // if put to 0 deletes also table heads. don't understand why
+    while (rowCount > 1) { // if put to 0 deletes also table heads. 
         myTable.deleteRow(1);
     }
     
