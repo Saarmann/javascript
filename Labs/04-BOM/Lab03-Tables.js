597f9c75025928b2733bd532d424cf8ccc41b2ba
saarmann <kaspar.saarmann@gmail.com>
2019-12-17 09:59:13 +0200

    [DAY-07]; table excercise buttons, clearTable() not working properly

--- Labs/04-BOM/Lab03-Tables.js
+++ Labs/04-BOM/Lab03-Tables.js
@@ -1,133 +1,79 @@
 alert ("Implement solution in Lab03-Tables.js file ");
 alert ("Test data connected: OrderNo: "+shipments[0].orderNo);
 
-var rows = 10;
+let newRows = 0;
 
 function loadTable() {
-
-let myTable = document.getElementById("myTable");
-const buttons = "<a href=\"#\" class=\"btn-action glyphicons eye_open btn-info\"><i></i></a><a href=\"#\" class=\"btn-action glyphicons pencil btn-success\"><i></i></a><a href=\"#\" class=\"btn-action glyphicons remove_2 btn-danger\"><i></i></a>"
-//document.getElementById("nextButton").addEventListener("click", );
- 
-
-let maxTableRows = shipments.length;
-var tableRows;
-
-if ((rows - 10) > (200 - 10)) {
-    var tableRows = maxTableRows;
-    var i = maxTableRows - 10;
-} else {
-    var tableRows = rows
-    var i = rows - 10;
-}
-
-var columns = [];
-for (let i = 0; i < shipments.length; i++) {
-    for(let key in shipments[i]){
-        if (columns.indexOf(key) === -1) {
-            columns.push(key);
+    let myTable = document.getElementById("myTable");
+    const buttons = "<a href=\"#\" class=\"btn-action glyphicons eye_open btn-info\"><i></i></a><a href=\"#\" class=\"btn-action glyphicons pencil btn-success\"><i></i></a><a href=\"#\" class=\"btn-action glyphicons remove_2 btn-danger\"><i></i></a>"
+    let rows = newRows + 10;
+    let maxTableRows = shipments.length;
+    let tableRows;
+    let i;
+    if ((rows - 10) > (200 - 10)) {
+        tableRows = maxTableRows;
+        i = maxTableRows - 10;
+    } else {
+        if (rows < 10) {
+            tablerows = 10;
+            i = tableRows - 10;
+        } else {
+            tableRows = rows
+            i = rows - 10;
         }
     }
-}
-
-// create table rows
-let tableRow = myTable.insertRow(); 
-    
-
+    var columns = [];
+    for (let i = 0; i < shipments.length; i++) {
+        for (let key in shipments[i]) {
+            if (columns.indexOf(key) === -1) {
+                columns.push(key);
+            }
+        }
+    }
+    // create table rows
+    let tableRow = myTable.insertRow();
     for (i; i < tableRows; i++) {
-
         tableRow = myTable.insertRow();
-
         // fill cells in table
-        for(let j = 0; j < (columns.length+1); j++ ) {
+        for (let j = 0; j < (columns.length + 1); j++) {
             let tableCell = tableRow.insertCell();
-            if(j == columns.length) {
+            if (j == columns.length) {
                 tableCell.innerHTML = buttons;
             } else {
                 tableCell.innerHTML = shipments[i][columns[j]];
-            }            
-        }          
-    }    
-
-
-}
-
-function loadTableNextPage() {
-
-let myTable = document.getElementById("myTable");
-const buttons = "<a href=\"#\" class=\"btn-action glyphicons eye_open btn-info\"><i></i></a><a href=\"#\" class=\"btn-action glyphicons pencil btn-success\"><i></i></a><a href=\"#\" class=\"btn-action glyphicons remove_2 btn-danger\"><i></i></a>"
-//document.getElementById("nextButton").addEventListener("click", );
- 
-var rows = 150;
-let maxTableRows = shipments.length;
-var tableRows;
-
-if ((rows - 10) > (200 - 10)) {
-    var tableRows = maxTableRows;
-    var i = maxTableRows - 10;
-} else {
-    var tableRows = rows
-    var i = rows - 10;
-}
-
-var columns = [];
-for (let i = 0; i < shipments.length; i++) {
-    for(let key in shipments[i]){
-        if (columns.indexOf(key) === -1) {
-            columns.push(key);
+            }
         }
     }
 }
-
-// create table rows
-let tableRow = myTable.insertRow(); 
-    
-
-    for (i; i < tableRows; i++) {
-
-        tableRow = myTable.insertRow();
-
-        // fill cells in table
-        for(let j = 0; j < (columns.length+1); j++ ) {
-            let tableCell = tableRow.insertCell();
-            if(j == columns.length) {
-                tableCell.innerHTML = buttons;
-            } else {
-                tableCell.innerHTML = shipments[i][columns[j]];
-            }            
-        }          
-    }    
-
-}
-
-
 function nextPage() {
-
-    var rows = rows + 10;
-    loadTableNextPage();
+    newRows = newRows + 10;
+    clearTable();
+    loadTable();
 }
-
 function previousPage() {
-
-
-
+    newRows = newRows - 10;
+    clearTable();
+    loadTable();
 }
-
-function lastPage () {
-
-
-
+function lastPage() {
+    newRows = shipments.length + 10;
+    clearTable();
+    loadTable();
 }
-
 function firstPage() {
-
-
-
+    newRows = 0;
+    clearTable();
+    loadTable();
 }
-
 function clearTable() {
-
-    document.getElementById("myTable");
+    let myTable = document.getElementById("myTable");
+    let rowCount = myTable.rows.length;
+    while (rowCount > 1) { // if put to 0 deletes also table heads. don't understand why
+        myTable.deleteRow(1);
+    }
     
-
 }
+
+
+
+
